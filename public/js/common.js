47ca612f3f807381bb71265e9356daa981d5995b
code1031 <1031247267@qq.com>
2017-09-18 12:32:19 +0800

    完善登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,36 @@
 
-	NProgress.start();
+NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+// 实现退出功能
+$('#logoutBtn').click(function(){
+  $.ajax({
+    type : 'post',
+    url : '/api/logout',
+    dataType : 'json',
+    success : function(data){
+      if(data.code == 200){
+        // 重新跳转到登录页
+        location.href = '/main/login';
+      }
+    }
+  });
+});
+
+// 检测用户是否登录
+var flag = $.cookie('PHPSESSID');
+if(!flag){
+  // 如果cookie不存在，跳转到登录页
+  location.href = '/main/login';
+}
+
+// 设置用户头像信息
+var loginInfo = $.cookie('loginInfo');
+loginInfo = loginInfo && JSON.parse(loginInfo);
+// 设置用户的头像信息
+$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+$('.aside .profile h4').html(loginInfo.tc_name);
