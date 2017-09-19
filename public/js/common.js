7562b9fd23b01746f7938a8875044ff4a9731bda
code1031 <1031247267@qq.com>
2017-09-19 18:07:28 +0800

    模板引擎重构登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
   // NProgress.start();
   // NProgress.done();
 
@@ -32,7 +32,9 @@ define(['jquery','cookie'],function($){
   var loginInfo = $.cookie('loginInfo');
   loginInfo = loginInfo && JSON.parse(loginInfo);
   // 设置用户的头像信息
-  $('.aside .profile img').attr('src',loginInfo.tc_avatar);
-  $('.aside .profile h4').html(loginInfo.tc_name);
-
+  // $('.aside .profile img').attr('src',loginInfo.tc_avatar);
+  // $('.aside .profile h4').html(loginInfo.tc_name);
+  var tpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div><h4>{{tc_name}}</h4>';
+  var html = template.render(tpl,loginInfo);
+  $('.aside .profile').html(html);
 });
--- public/js/config.js
+++ public/js/config.js
@@ -3,6 +3,7 @@ require.config({
   paths : {
     jquery : 'jquery/jquery',
     cookie : 'jquery-cookie/jquery.cookie',
+    template : 'artTemplate/template-web',
     common : '../js/common',
     login : '../js/login'
   }
