a0a14aefddc3071840d440280f02ffbd38fcb67e
code1031 <1031247267@qq.com>
2017-09-21 17:04:59 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -12,12 +12,32 @@ define(['jquery','template','util'],function($,template,util){
         data.result.operate = '编辑讲师';
         var html = template('teacherTpl',data.result);
         $('#teacherInfo').html(html);
+        // 处理表单提交
+        submitForm('/api/teacher/update');
       }
     });
   }else{
     // 添加操作
     var html = template('teacherTpl',{operate:'添加讲师'});
     $('#teacherInfo').html(html);
+    submitForm('/api/teacher/add');
+  }
+
+  // 实现表单提交
+  function submitForm(url){
+    $('#teacherBtn').click(function(){
+      $.ajax({
+        type : 'post',
+        url : url,
+        data : $('#teacherForm').serialize(),
+        dataType : 'json',
+        success : function(data){
+          if(data.code == 200){
+            location.href = '/teacher/list';
+          }
+        }
+      });
+    });
   }
 
 
