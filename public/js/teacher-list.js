0a999adbfb371e1b7ca881b9df8c52292f7ee494
code1031 <1031247267@qq.com>
2017-09-21 16:01:56 +0800

    完成启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -8,6 +8,33 @@ define(['jquery','template'],function($,template){
       // 解析数据，渲染页面
       var html = template('teacherTpl',{list:data.result});
       $('#teacherInfo').html(html);
+
+      // 启用注销功能
+      $('.eod').click(function(){
+        var that = this;
+        var td = $(this).closest('td');
+        var tcId = td.attr('data-tcId');
+        var status = td.attr('data-status');
+        $.ajax({
+          type : 'post',
+          url : '/api/teacher/handle',
+          data : {tc_id : tcId,tc_status : status},
+          dataType : 'json',
+          success : function(data){
+            if(data.code == 200){
+              td.attr('data-status',data.result.tc_status);
+              if(data.result.tc_status == 0){
+                $(that).text('注销');
+              }else{
+                $(that).text('启用');
+              }
+            }
+          }
+        });
+      });
+
     }
   });
+
+
 });
