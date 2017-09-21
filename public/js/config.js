348ad3d08f058bfc3849d7d249aba7b46ff28126
code1031 <1031247267@qq.com>
2017-09-21 17:43:05 +0800

    添加日期插件

--- public/js/config.js
+++ public/js/config.js
@@ -5,6 +5,8 @@ require.config({
     cookie : 'jquery-cookie/jquery.cookie',
     template : 'artTemplate/template-web',
     bootstrap : 'bootstrap/js/bootstrap.min',
+    datepicker : 'bootstrap-datepicker/js/bootstrap-datepicker',
+    language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
     util : '../js/util',
     common : '../js/common',
     login : '../js/login',
@@ -14,6 +16,9 @@ require.config({
   shim : {
     bootstrap : {
       deps : ['jquery']
+    },
+    language : {
+      deps : ['jquery','datepicker']
     }
   }
 });
--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','datepicker','language'],function($,template,util){
   var tcId = util.qs('tc_id');
   if(tcId){
     // 编辑操作
