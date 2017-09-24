2d50179a487171ae153c2d55cd7057d7ab335f95
code1031 <1031247267@qq.com>
2017-09-24 16:51:21 +0800

    完成课程列表展示功能

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,4 +1,15 @@
 define(['jquery','template','util'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu(location.pathname);
+  // 获取所有的课程列表数据
+  $.ajax({
+    type : 'get',
+    url : '/api/course',
+    dataType : 'json',
+    success : function(data){
+      // 解析数据，渲染页面
+      var html = template('courseTpl',{list : data.result});
+      $('#courseInfo').html(html);
+    }
+  });
 });
