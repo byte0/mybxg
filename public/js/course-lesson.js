0e12be58df1c49918054e3f78db155ca4f95c08d
code1031 <1031247267@qq.com>
2017-09-27 15:52:35 +0800

    完成课时添加和编辑的弹窗共享

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu('/course/add');
   // 获取课程ID
@@ -13,6 +13,35 @@ define(['jquery','template','util'],function($,template,util){
       // 解析数据，渲染页面
       var html = template('lessonTpl',data.result);
       $('#lessonInfo').html(html);
+      // 绑定添加课时事件
+      $('#addLesson').click(function(){
+        // 解析数据，渲染模态框
+        var html = template('modalTpl',{operate : '添加课时'});
+        $('#modalInfo').html(html);
+        // 显示模态框
+        $('#chapterModal').modal();
+      });
+      // 绑定编辑课时事件
+      $('.editBtn').click(function(){
+        // 获取课时ID
+        var ctId = $(this).attr('data-ctId');
+        // 根据课时ID查询课时数据信息
+        $.ajax({
+          type : 'get',
+          url : '/api/course/chapter/edit',
+          data : {ct_id : ctId},
+          dataType : 'json',
+          success : function(data){
+            // 解析数据，渲染模态框
+            data.result.operate = '编辑课时';
+            var html = template('modalTpl',data.result);
+            $('#modalInfo').html(html);
+            // 显示模态框
+            $('#chapterModal').modal();
+          }
+        });
+        
+      });
     }
   });
 
